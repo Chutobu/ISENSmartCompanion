2ae0e4b28540bae17f7c143391d145964caff5c2
Chutobu <tombouchut18@gmail.com>
2025-03-09 17:49:41 +0100

    initial commit

---
+++ app/src/main/java/fr/isen/bouchut/isensmartcompanion/screens/mainScreen.kt
@@ -0,0 +1,90 @@
+package fr.isen.bouchut.isensmartcompanion.screens
+
+
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import fr.isen.bouchut.isensmartcompanion.R
+import fr.isen.bouchut.isensmartcompanion.ui.theme.ISENSmartCompanionTheme
+import fr.isen.bouchut.isensmartcompanion.viewmodel.MainViewModel
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    val viewModel: MainViewModel = viewModel()
+    val context = LocalContext.current
+    var userInput = remember { mutableStateOf("") }
+    Column(modifier = modifier
+        .fillMaxSize()
+        .padding(start = 15.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.isen),
+            contentDescription = stringResource(id = R.string.title_image),
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .height(200.dp),
+            contentScale = ContentScale.Fit
+        )
+
+        Text(context.getString(R.string.app_name) )
+//        Text("Smart Companion")
+        Text(stringResource(id=R.string.Welcoming_message))
+        Spacer(Modifier.weight(0.5f))
+
+        Row {
+            TextField(userInput.value, { newValue ->
+                userInput.value = newValue
+            })
+            Button(
+                onClick = {
+                    val content = userInput.value.trim()
+                    if (content.isNotEmpty()) {
+                        viewModel.addHistoryEntry(content)
+                        userInput.value = ""
+                        Toast.makeText(context, R.string.add_history, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.send), // Image du bouton
+                    contentDescription = stringResource(id = R.string.button)
+                )
+            }
+        }
+        }
+    }
+
